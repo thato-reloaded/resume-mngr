252ab245da8e5c2b0d541f3668e96f3ff427a37d
ThatoR <thatoramphore.info@gmail.com>
2025-12-01 16:49:03 +0200

    feat(theme-context): add theme context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useContext } from 'react';
+import { ThemeContext } from "./context/theme.context";
 
 const App = () => {
+  const {darkMode} = useContext(ThemeContext);
+
+  const appStyles = darkMode ? "app dark" : "app"; 
+
   return (
-    <div className="app">App</div>
+    <div className={appStyles}>App</div>
   )
 }
 
-export default App
+export default App 
